c8abcc7b25fd224a94dd023261d804124163bd37
Thomas Rausch <thomas@thrau.at>
2021-10-18 00:18:49 +0200

    import code

---
+++ plugin/__init__.py
@@ -0,0 +1,26 @@
+from .core import (
+    Plugin,
+    PluginDisabled,
+    PluginException,
+    PluginFinder,
+    PluginLifecycleListener,
+    PluginSpec,
+    PluginType,
+)
+from .manager import PluginManager, PluginSpecResolver
+
+name = "plugin"
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "Plugin",
+    "PluginSpec",
+    "PluginType",
+    "PluginLifecycleListener",
+    "PluginFinder",
+    "PluginManager",
+    "PluginSpecResolver",
+    "PluginException",
+    "PluginDisabled",
+]
