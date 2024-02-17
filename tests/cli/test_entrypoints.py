d46c64a524200894298444f39074b859338ee45c
Thomas Rausch <thomas@thrau.at>
2024-02-17 18:55:46 +0100

    fix test

--- tests/cli/test_entrypoints.py
+++ tests/cli/test_entrypoints.py
@@ -6,6 +6,9 @@ import pytest
 
 @pytest.mark.parametrize("project_name", ["pyproject", "setupcfg"])
 def test_entrypoints(project_name):
+    if project_name == "pyproject" and sys.version_info < (3, 10):
+        pytest.xfail("reading pyproject.toml requires Python 3.10 or above")
+
     from plux.__main__ import main
 
     project = os.path.join(os.path.dirname(__file__), "projects", project_name)
@@ -21,4 +24,5 @@ def test_entrypoints(project_name):
         sys.path.remove(project)
 
     with open(os.path.join(project, "test_project.egg-info", "entry_points.txt"), "r") as f:
-        assert f.readlines() == ["[plux.test.plugins]\n", "myplugin = mysrc.plugins:MyPlugin\n"]
+        lines = [line.strip() for line in f.readlines() if line.strip()]
+        assert lines == ["[plux.test.plugins]", "myplugin = mysrc.plugins:MyPlugin"]
